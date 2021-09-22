910a9f156e376147dcf2f3a9154c1c8cf7f49cbe
Singstad90 <bjorn_sing@hotmail.com>
2021-09-22 10:30:21 +0200

    New loss

--- CNN_Model.py
+++ CNN_Model.py
@@ -240,6 +240,33 @@ def plot_normalized_conf_matrix_dev(y_pred, ecg_filenames, y, val_fold, threshol
     sns.set(font_scale=1.4)
     sns.heatmap(df_norm_col, cmap="Blues", annot=True, annot_kws={"size": 16}, fmt=".2f", cbar=False)  # font size
 
+# F1 loss - This function makes the threshold optimization unnecessary
+#https://towardsdatascience.com/multi-label-image-classification-in-tensorflow-2-0-7d4cf8a4bc72
+def macro_double_soft_f1(y_true, y_pred):
+    """Compute the macro soft F1-score as a cost (average 1 - soft-F1 across all labels).
+    Use probability values instead of binary predictions.
+    This version uses the computation of soft-F1 for both positive and negative class for each label.
+    
+    Args:
+        y (int32 Tensor): targets array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
+        
+    Returns:
+        cost (scalar Tensor): value of the cost function for the batch
+    """
+    y = tf.cast(y_true, tf.float32)
+    y_hat = tf.cast(y_pred, tf.float32)
+    tp = tf.reduce_sum(y_hat * y, axis=0)
+    fp = tf.reduce_sum(y_hat * (1 - y), axis=0)
+    fn = tf.reduce_sum((1 - y_hat) * y, axis=0)
+    tn = tf.reduce_sum((1 - y_hat) * (1 - y), axis=0)
+    soft_f1_class1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    soft_f1_class0 = 2*tn / (2*tn + fn + fp + 1e-16)
+    cost_class1 = 1 - soft_f1_class1 # reduce 1 - soft-f1_class1 in order to increase soft-f1 on class 1
+    cost_class0 = 1 - soft_f1_class0 # reduce 1 - soft-f1_class0 in order to increase soft-f1 on class 0
+    cost = 0.5 * (cost_class1 + cost_class0) # take into account both class 1 and class 0
+    macro_cost = tf.reduce_mean(cost) # average on all labels
+    return macro_cost
 ###############################################################################################
 
 print("type in your data path:")
@@ -319,6 +346,10 @@ lenet_5_model.add(Dense(64, activation='relu'))
 lenet_5_model.add(Dense(32, activation='relu'))
 
 lenet_5_model.add(Dense(num_classes, activation = 'sigmoid'))
+
+# Uncomment this code and comment out the other line below to use the new loss func
+#lenet_5_model.compile(loss=macro_double_soft_f1, optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), metrics=[tf.keras.metrics.BinaryAccuracy(
+#       name='accuracy', dtype=None, threshold=0.5)])
 
 lenet_5_model.compile(loss=tf.keras.losses.BinaryCrossentropy(), optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), metrics=[tf.keras.metrics.BinaryAccuracy(
         name='accuracy', dtype=None, threshold=0.5)])
